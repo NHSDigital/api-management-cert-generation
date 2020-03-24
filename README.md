7100197f0fa9ae98ffcf99700afaf399c39f80bd
Laurence Smith <laurence.smith@airelogic.com>
2020-03-24 10:07:20 +0000

    APM-408 Greatly simplify generation

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 # API Management Cert Generation
 
 ## Prerequesites
-* Python 3
 * bash
 * openssl cli
 
 ## How to use this
-### 1. Generate some keys
-Run `create_keys.sh` to generate some 256-bit Ed25519 keys into `keys/`.
-
-### 2. Generate some configs
-Run `python create_confs.py` to generate confs from the template into `confs/`.
-
-### 3. Generate CSRs
-Run `create_csrs.sh` to generate CSRs into `csrs/` to send to a trusted CA.
+### 1. Generate CSRs
+Run `create_csrs.sh` to generate keys into `keys/` and CSRs into `csrs/` to send to a trusted CA.
 
 ## How to check your work
 To analyze a CSR you can use `openssl req -in CSR_PATH -text -noout`.
+
+Alternatively if your system supports it, double-click on a CSR to view the same info in a more friendly format.
--- create_csrs.sh
+++ create_csrs.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 mkdir -p csrs
+mkdir -p keys
 
-while read -r line;
-do
-    openssl req -new -out csrs/$line.csr -key keys/$line.key -config confs/$line.conf
-done < domains.txt
+openssl req -new -newkey rsa:4096 -nodes -sha256 -keyout keys/internal-dev.api.service.nhs.uk.key -out csrs/internal-dev.api.service.nhs.uk.csr -config confs/internal.conf -extensions v3_req
+openssl req -new -newkey rsa:4096 -nodes -sha256 -keyout keys/dev.api.service.nhs.uk.key -out csrs/dev.api.service.nhs.uk.csr -config confs/dev.conf -extensions v3_req
+openssl req -new -newkey rsa:4096 -nodes -sha256 -keyout keys/int.api.service.nhs.uk.key -out csrs/int.api.service.nhs.uk.csr -config confs/int.conf -extensions v3_req
+openssl req -new -newkey rsa:4096 -nodes -sha256 -keyout keys/api.service.nhs.uk.key -out csrs/api.service.nhs.uk.csr -config confs/prod.conf -extensions v3_req
